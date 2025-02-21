d70c3f63b89338b3272cfad5fe9e808b92a72143
Jhon Edinson Muñoz Riaños <je.munozr1@uniandes.edu.co>
2025-02-21 01:46:30 -0500

    ajusta importe 2

--- src/aeroalpes/modulos/ingesta/infraestructura/mapeadores.py
+++ src/aeroalpes/modulos/ingesta/infraestructura/mapeadores.py
@@ -20,25 +20,10 @@ class MapeadorIngesta(Mapeador):
         ingesta_dto = IngestaDTO()
         ingesta_dto.id = str(entidad.id)
 
-        reserva_dto.fecha_creacion = entidad.fecha_creacion
-        reserva_dto.fecha_actualizacion = entidad.fecha_actualizacion
-        reserva_dto.id = str(entidad.id)
-
-        itinerarios_dto = list()
-        
-        for itinerario in entidad.itinerarios:
-            itinerarios_dto.extend(self._procesar_itinerario(itinerario))
-
-        reserva_dto.itinerarios = itinerarios_dto
-
         return ingesta_dto
 
-    def dto_a_entidad(self, dto: ReservaDTO) -> Reserva:
-        reserva = Reserva(dto.id, dto.fecha_creacion, dto.fecha_actualizacion)
+    def dto_a_entidad(self, dto: IngestaDTO) -> Ingesta:
+        reserva = Ingesta(dto.id, dto.fecha_creacion, dto.fecha_actualizacion)
         reserva.itinerarios = list()
 
-        itinerarios_dto: list[ItinerarioDTO] = dto.itinerarios
-
-        reserva.itinerarios.extend(self._procesar_itinerario_dto(itinerarios_dto))
-        
         return reserva
