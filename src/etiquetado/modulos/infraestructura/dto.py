0892d5f2ac75aedf7348f124115d558423d04b81
Jhon Edinson Muñoz Riaños <je.munozr1@uniandes.edu.co>
2025-03-02 02:11:03 -0500

    fix uow

--- src/etiquetado/modulos/infraestructura/dto.py
+++ src/etiquetado/modulos/infraestructura/dto.py
@@ -5,18 +5,18 @@ la infraestructura del dominio de vuelos
 
 """
 
-from etiquetado.config.db import db
+from etiquetado.config.db import db, Base
 from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy import Column, ForeignKey, Integer, Table
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 
 import uuid
 
-Base = db.declarative_base()
 
 
-class Imagen(db.Model):
+
+class Imagen(Base):
     __tablename__ = "imagenes"
-    id = db.Column(db.String(40), primary_key=True)
-    id_ingesta = db.Column(db.String(40), nullable=False)
-    url_path = db.Column(db.String(200), nullable=False)
-    estado = db.Column(db.String(40), nullable=False)
+    id = Column(String(40), primary_key=True)
+    id_ingesta = Column(String(40), nullable=False)
+    url_path = Column(String(200), nullable=False)
+    estado = Column(String(40), nullable=False)
