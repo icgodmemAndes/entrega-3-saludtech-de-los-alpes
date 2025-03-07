55a815043f64fe3e7670ba8df02f948a0afbc8a4
Jhon Edinson Muñoz Riaños <je.munozr1@uniandes.edu.co>
2025-03-06 22:38:48 -0500

    fix init

--- src/etiquetado/modulos/etiquetado/aplicacion/__init__.py
+++ src/etiquetado/modulos/etiquetado/aplicacion/__init__.py
@@ -2,8 +2,8 @@ from pydispatch import dispatcher
 
 from .handlers import HandlerEtiquetadoIntegracion,HandlerRevertirIntegracion
 
-from etiquetado.modulos.etiquetado.dominio.eventos import EtiquetadoCreada, Revertir
+from etiquetado.modulos.etiquetado.dominio.eventos import EtiquetadoCreada, RevertirEtiquetado
 
 dispatcher.connect(HandlerEtiquetadoIntegracion.handle_etiquetado_creada, signal=f'{EtiquetadoCreada.__name__}Integracion')
 
-dispatcher.connect(HandlerRevertirIntegracion.handle_etiquetado_revertir, signal=f'{Revertir.__name__}Integracion')
+dispatcher.connect(HandlerRevertirIntegracion.handle_etiquetado_revertir, signal=f'{RevertirEtiquetado.__name__}Integracion')
